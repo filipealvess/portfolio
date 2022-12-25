27e298c13550351bde7b3db2667afa6ff2cb2464
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-12-25 20:24:15 -0300

    feat: componente Experience

---
+++ src/components/Experience/index.tsx
@@ -0,0 +1,91 @@
+import Image from 'next/image';
+import React, { useEffect, useRef, useState } from 'react';
+import { ChevronDown, ChevronUp } from 'react-feather';
+import ExperienceProps from '../../types/ExperienceProps';
+import {
+  Container,
+  Content,
+  Description,
+  Grid,
+  Header,
+  ImageContainer,
+  List,
+  Time,
+  Titles,
+  Tool
+} from './styles';
+
+interface Props {
+  experience: ExperienceProps
+}
+
+export default function Experience({ experience }: Props) {
+  const [isExpanded, setIsExpanded] = useState(true);
+  const [contentHeight, setContentHeight] = useState<string | null>(null);
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    if (contentRef && contentRef.current) {
+      const height = window.getComputedStyle(contentRef.current).height;
+
+      setContentHeight(height);
+      setIsExpanded(false);
+    }
+  }, [contentRef]);
+
+  function toggle() {
+    setIsExpanded(prevState => !prevState);
+  }
+
+  return (
+    <Container>
+      <Header onClick={toggle}>
+        <ImageContainer className="image">
+          <Image src={experience.cover} fill alt={experience.subtitle} />
+        </ImageContainer>
+
+        <Titles>
+          <h4>{experience.title}</h4>
+          <p>{experience.subtitle}</p>
+        </Titles>
+
+        {isExpanded ? <ChevronUp /> : <ChevronDown />}
+      </Header>
+
+      <Content visible={isExpanded} ref={contentRef} height={contentHeight}>
+        <Time>{experience.start} - {experience.end}</Time>
+
+        {experience.descriptions.map((description, index) => (
+          <Description key={index}>{description}</Description>
+        ))}
+
+        <List>
+          <h5>Habilidades adquiridas/melhoradas:</h5>
+
+          <ul>
+            {experience.skills.map((skill, index) => (
+              <li key={index}>{skill}</li>
+            ))}
+          </ul>
+        </List>
+
+        <List>
+          <h5>Ferrametas utilizadas:</h5>
+
+          <Grid>
+            {experience.tools.map((tool, index) => (
+              <Tool
+                key={index}
+                width={40}
+                height={40}
+                src={tool.image}
+                alt={tool.name}
+                title={tool.name}
+              />
+            ))}
+          </Grid>
+        </List>
+      </Content>
+    </Container>
+  );
+}
---
+++ src/components/Experience/styles.ts
@@ -0,0 +1,133 @@
+import Image from 'next/image';
+import styled, { css } from 'styled-components';
+
+interface ContentProps {
+  visible: boolean;
+  height: string | null;
+}
+
+export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  border: 1px solid ${({ theme }) => theme.borderColor};
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.sectionBackground};
+
+  & ~ & {
+    margin-top: 16px;
+  }
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 24px;
+  cursor: pointer;
+
+  &:hover .image {
+    transform: scale(1.05);
+  }
+
+  @media (max-width: 500px) {
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+`;
+
+export const ImageContainer = styled.div`
+  position: relative;
+  width: 80px;
+  height: 80px;
+  border: 1px solid ${({ theme }) => theme.borderColor};
+  border-radius: 8px;
+  transition: 0.3s transform;
+
+  img {
+    border-radius: 8px;
+  }
+`;
+
+export const Titles = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  flex: 1;
+  margin: 0 24px;
+  
+  @media (max-width: 500px) {
+    order: 3;
+    min-width: 100%;
+    margin: 16px 0 0;
+  }
+`;
+
+export const Content = styled.section`
+  position: relative;
+  padding: 24px;
+  text-align: left;
+  overflow: hidden;
+  transition-duration: 0.3s;
+  transition-property: height, padding;
+
+  ${({ height }) => height && css`
+    height: ${height};
+  `}
+
+  ${({ visible }: ContentProps) => !visible && css`
+    height: 0;
+    padding: 0 24px;
+  `}
+
+  &::after {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 24px;
+    width: calc(100% - 48px);
+    height: 1px;
+    background-color: ${({ theme }) => theme.borderColor};
+  }
+`;
+
+export const Time = styled.p`
+  margin-bottom: 24px;
+  font-style: italic;
+`;
+
+export const Description = styled.p`
+  & ~ & {
+    margin-top: 16px;
+  }
+`;
+
+export const List = styled.article`
+  margin-top: 24px;
+
+  h5 {
+    margin-bottom: 8px;
+    font-weight: 600;
+  }
+
+  li::before {
+    content: '- ';
+  }
+`;
+
+export const Grid = styled.footer`
+  display: flex;
+  gap: 8px;
+  flex-wrap: wrap;
+`;
+
+export const Tool = styled(Image)`
+  border: 1px solid ${({ theme }) => theme.borderColor};
+  border-radius: 8px;
+  transition: 0.3s transform;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
