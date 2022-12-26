6a4b04a40ad02ac683fe2da0d3ac15f4c095396f
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-12-26 14:28:47 -0300

    feat(static): idiomas

---
+++ src/static/languages.ts
@@ -0,0 +1,21 @@
+import LanguageProps from '../types/languageProps';
+
+const languages: LanguageProps[] = [
+  {
+    name: 'Português',
+    level: 'Fluente/Nativo',
+    points: 5
+  },
+  {
+    name: 'Inglês',
+    level: 'Intermediário',
+    points: 3
+  },
+  {
+    name: 'Espanhol',
+    level: 'Básico',
+    points: 1
+  },
+];
+
+export default languages;
---
+++ src/types/languageProps.ts
@@ -0,0 +1,7 @@
+interface LanguageProps {
+  name: string;
+  level: string;
+  points: 0 | 1 | 2 | 3 | 4 | 5;
+}
+
+export default LanguageProps;
